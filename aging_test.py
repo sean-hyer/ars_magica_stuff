50a01f482997bbe6df41b54433d56834132877c2
sean-hyer <56715695+sean-hyer@users.noreply.github.com>
2025-07-14 13:29:44 -0400

    Update aging_test.py

--- aging_test.py
+++ aging_test.py
@@ -1,17 +1,20 @@
-# so
-# aging
-# stress die (no botch)
-# plus age/10 (round up)
-# -  modifiers
-# we'll ignore random point increases
-# your age increases by 1 every time you roll a 13 or a 22+
-# we'll assume medical care is always perfect and available
+'''
+The way aging works in Ars Magica:
+Every year once you turn 35:
+Roll a stress die (no botch)
+Add your age/10 (round up)
+Subtract any modifiers
+If the result is a 13 or 22+, gain a decrepitude (a point of aging: you die at 5)
+'''
 
 import random
 import math
 
+num_tries = 10000
 
 def stress_roll():
+    '''This function rolls a stress die and returns the result.
+    If the stress die multiplies, it calls a second function for recursion.'''
     roll = random.randint(1, 10)
     if roll == 10:
         return 0
@@ -21,6 +24,7 @@ def stress_roll():
 
 
 def multiply_roll(multiplier):
+    '''This function implements stress die explosion'''
     roll = random.randint(1, 10)
     if roll == 1:
         roll = multiply_roll(multiplier*2)
@@ -39,16 +43,17 @@ def aging(modifier):
             decrepitude += 1
     return age
 
-
+# Right now I'm not using this.
+# It might be useful later but I'm looking at averages not distributions.
 age_grid = []
 for i in range(0, 300):
     age_grid.append([(i), (0)])
 
 sum_ages = 0
-for i in range(1, 10000):
+for i in range(1, num_tries):
     result = aging(8)
     age_grid[result][1] += 1
     sum_ages += result
 
 print(age_grid)
-print(f"the average age at death was {sum_ages / 10000}")
+print(f"the average age at death was {sum_ages / num_tries}")
